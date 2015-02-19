cf3510b825e31a9d74867d227690d5ea6f8f1b44
jfmm <moralejf@gmail.com>
2015-02-18 21:27:56 -0500

    experimenting with svg and d3

--- js/app.js
+++ js/app.js
@@ -128,3 +128,41 @@ var jsonReport, updatedOn, sol, minTemp, maxTemp, minTempF, maxTempF, condition;
 
 
 
+
+
+/*=======================================
+*	D3.js Visualitzation Code
+*=======================================*/
+var dataset = [
+	{"date":"2012-03-20","total":3},
+	{"date":"2012-03-21","total":8},
+	{"date":"2012-03-22","total":-42},
+	{"date":"2012-03-23","total":-3},
+	{"date":"2012-03-24","total":3},
+	{"date":"2012-03-25","total":20},
+	{"date":"2012-03-26","total":12}
+];
+
+// svg element dimensions
+var w = 500,
+		h = 300;
+
+ //create svg element
+var svg = d3.select("#temp-graph").append("svg").attr("width", w).attr("height", h);
+
+
+var circles = svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+		.style("fill", "#E95124");
+
+
+ circles.attr("cx", function(d, i) {
+            return (i * 50) + 25;
+        })
+       .attr("cy", h/2)
+       .attr("r", function(d) {
+            return d.total;
+       });
+
