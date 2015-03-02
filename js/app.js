7a044d5e3fc4c95f223ae2e188302184efe4d04a
Jose Morales-mendizabal <moralejf@gmail.com>
2015-03-02 17:12:40 -0500

    adds orbit module

--- js/app.js
+++ js/app.js
@@ -553,3 +553,162 @@ function drawChart(tempUnit, loadCached, archiveKey) {
 		 .call(yAxis);
 
 }
+
+
+
+
+/**=------------------------------------------
+*	ORBIT VIZUALIZATIO CODE
+*-------------------------------------------*/
+
+
+
+var earthOrbitPosition, marsOrbitPosition, day, radii;
+
+function drawSpaceTime() {
+
+	var now = new Date(d3.time.year.floor(new Date()));
+
+	var spacetime = d3.select('#spacetime-wrap');
+	var width = 500,
+			height = 500,
+			radius = Math.min(width, height);
+
+	radii = {
+		"sun": radius / 8,
+		"earthOrbit": radius / 3.3,
+		"earth": radius / 32,
+		"marsOrbit": radius / 2.2,
+		"mars": radius / 48
+	};
+
+	// Space
+	var svg = spacetime.append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.append("g")
+			.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+	// Sun
+	svg.append("circle")
+		.attr("class", "sun")
+		.attr("r", radii.sun)
+		.style("fill", "rgba(255, 204, 0, 1.0)");
+
+	// Earth's orbit
+	svg.append("circle")
+		.attr("class", "earthOrbit")
+		.attr("r", radii.earthOrbit)
+		.style("fill", "none")
+		.style("stroke", "rgba(255, 204, 0, 0.25)");
+
+	// Current position of Earth in its orbit
+	earthOrbitPosition = d3.svg.arc()
+		.outerRadius(radii.earthOrbit + 1)
+		.innerRadius(radii.earthOrbit - 1)
+		.startAngle(0)
+		.endAngle(0);
+	svg.append("path")
+		.attr("class", "earthOrbitPosition")
+		.attr("d", earthOrbitPosition)
+		.style("fill", "rgba(255, 204, 0, 0.75)");
+
+	// Earth
+	svg.append("circle")
+		.attr("class", "earth")
+		.attr("r", radii.earth)
+		.attr("transform", "translate(0," + -radii.earthOrbit + ")")
+		.style("fill", "rgba(113, 170, 255, 1.0)");
+
+	// Time of day
+	day = d3.svg.arc()
+		.outerRadius(radii.earth)
+		.innerRadius(0)
+		.startAngle(0)
+		.endAngle(0);
+	svg.append("path")
+		.attr("class", "day")
+		.attr("d", day)
+		.attr("transform", "translate(0," + -radii.earthOrbit + ")")
+		.style("fill", "rgba(53, 110, 195, 1.0)");
+
+
+
+	// Mar's orbit
+	svg.append("circle")
+		.attr("class", "marsOrbit")
+		.attr("r", radii.marsOrbit)
+		.style("fill", "none")
+		.style("stroke", "rgba(237, 108, 39, 0.25)");
+
+	// Current position of the Mars in its orbit
+	marsOrbitPosition = d3.svg.arc()
+		.outerRadius(radii.marsOrbit + 1)
+		.innerRadius(radii.marsOrbit - 1)
+		.startAngle(0)
+		.endAngle(0);
+	svg.append("path")
+		.attr("class", "marsOrbitPosition")
+		//.attr("d", marsOrbitPosition(now))
+		.attr("d", marsOrbitPosition)
+		.style("fill", "rgba(255, 37, 37, 0.75)");
+
+	// Mars
+	svg.append("circle")
+		.attr("class", "mars")
+		.attr("r", radii.mars)
+		.attr("transform", "translate(0," + -radii.marsOrbit + ")")
+		.style("fill", "#E95124");
+
+}
+
+
+
+
+// Update the clock every second
+setInterval(function () {
+  now = new Date();
+  
+  var interpolateEarthOrbitPosition = d3.interpolate(earthOrbitPosition.endAngle()(), (2 * Math.PI * d3.time.hours(d3.time.year.floor(now), now).length / d3.time.hours(d3.time.year.floor(now), d3.time.year.ceil(now)).length)); //the length of hours from start of earth year and today / number of hours between today and the end of the year
+	
+	console.log(d3.time.hours(d3.time.year.floor(now), d3.time.year.ceil(now)).length);
+	
+	var interpolateMarsOrbitPosition = d3.interpolate(marsOrbitPosition.endAngle()(), (2 * Math.PI * (1500 / 2000) ));
+  
+  //var interpolateDay = d3.interpolate(day.endAngle()(), (2 * Math.PI * d3.time.seconds(d3.time.day.floor(now), now).length / d3.time.seconds(d3.time.day.floor(now), d3.time.day.ceil(now)).length));
+  
+  
+  d3.transition().tween("orbit", function () {
+    return function (t) {
+      // Animate Earth orbit position
+      d3.select(".earthOrbitPosition").attr("d", earthOrbitPosition.endAngle(interpolateEarthOrbitPosition(t)));
+
+      // Transition Earth
+      d3.select(".earth")
+        .attr("transform", "translate(" + radii.earthOrbit * Math.sin(interpolateEarthOrbitPosition(t) - earthOrbitPosition.startAngle()()) + "," + -radii.earthOrbit * Math.cos(interpolateEarthOrbitPosition(t) - earthOrbitPosition.startAngle()()) + ")");
+			
+			
+			
+			// Animate Mars Orbit Position
+			 d3.select(".marsOrbitPosition").attr("d", marsOrbitPosition.endAngle(interpolateMarsOrbitPosition(t)));
+			
+			// transition Mars
+			d3.select('.mars')
+				.attr("transform",  "translate(" + radii.marsOrbit * Math.sin(interpolateMarsOrbitPosition(t) - marsOrbitPosition.startAngle()()) + "," + -radii.marsOrbit * Math.cos(interpolateMarsOrbitPosition(t) - marsOrbitPosition.startAngle()()) + ")");
+			
+
+//      // Animate day
+//      // Transition day
+//      d3.select(".day")
+//        .attr("d", day.endAngle(interpolateDay(t)))
+//        .attr("transform", "translate(" + radii.earthOrbit * Math.sin(interpolateEarthOrbitPosition(t) - earthOrbitPosition.startAngle()()) + "," + -radii.earthOrbit * Math.cos(interpolateEarthOrbitPosition(t) - earthOrbitPosition.startAngle()()) + ")");
+      
+ 
+    };
+  });
+
+
+}, 1000);
+
+
+drawSpaceTime();
